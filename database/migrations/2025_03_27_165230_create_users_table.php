dc99565fdad266ee9644295cc5b6e906e24cf51f
Iuri <iurilugao01@gmail.com>
2025-04-01 08:27:24 -0300

    first commit

---
+++ database/migrations/2025_03_27_165230_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 50);
+            $table->string('emial', 50);
+            $table->string('password', 20);
+            $table->integer('phone');
+            $table->string('cargo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
